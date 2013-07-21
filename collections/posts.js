aec26d819dd9bf168a5608a669986cbd1049b0fb
David Brear <davidbrear04@gmail.com>
2013-07-20 20:07:28 -0500

    got the submit page working and fixed the ordering

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,34 @@
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+  insert: function(userId, doc){
+    return !! userId;
+  }
+});
+
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+    postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+    if (!user)
+      throw new Meteor.Error(401, 'You need to be logged in to do that');
+
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'You did not give this post a title');
+
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, 'This link has already been submitted', postWithSameLink._id);
+    }
+
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
+  }
+});
